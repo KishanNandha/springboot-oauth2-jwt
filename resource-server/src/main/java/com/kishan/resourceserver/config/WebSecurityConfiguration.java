8c3b5b12103c2093fe82901a3b08ea3895bdd1fd
KishanNandha <kishan.nandha99@gmail.com>
2020-08-03 01:07:08 +0530

    Added auth server and resource server.

---
+++ resource-server/src/main/java/com/kishan/resourceserver/config/WebSecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.kishan.resourceserver.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration {
+
+}
